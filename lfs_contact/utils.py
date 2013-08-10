a990398612d140af30cb60cabe366d8260e905db
tzicatl <nnieto@noenieto.com>
2013-08-09 17:13:00 -0700

    Do not use user's email as from_addr

--- lfs_contact/utils.py
+++ lfs_contact/utils.py
@@ -6,6 +6,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
 # lfs imports
 import lfs.core.utils
@@ -34,7 +35,14 @@ def send_contact_mail(request, form, template="lfs/mail/contact_mail.html"):
         "fields": fields,
     }))
 
-    mail = EmailMultiAlternatives(subject=subject, body="", from_email=from_email, to=to, bcc=bcc)
+    mail = EmailMultiAlternatives(
+        subject=subject,
+        body="", 
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=to, 
+        bcc=bcc,
+        headers = {'Reply-To': 'from_email'}
+    )
     mail.attach_alternative(text, "text/html")
 
     mail.send()
